e5fba267f7f3fbde62048cefbe2a8592c1295cee
Ashley Davis <dual@gmx.com>
2014-11-14 22:34:32 +0000

    Add response via node 'request' library

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 var private = require("./private.js");
+var request = require("request")
 var WebSocketClient = require('websocket').client;
 
 var client = new WebSocketClient();
+
 
 client.on('connectFailed', function(error) {
     console.log('Connect Error: ' + error.toString());
@@ -19,6 +21,18 @@ client.on('connect', function(connection) {
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             console.log("Received: '" + message.utf8Data + "'");
+        }
+        var msg = JSON.parse(message.utf8Data);
+        if(msg.type=="tickle"){
+          request({
+            uri: private.slackURI,
+            method: "POST",
+            form: {
+              payload: '{"text": "got push..."}'
+            }
+          }, function(error, response, body) {
+              console.log("Response: "+body);
+          });
         }
     });
 });
