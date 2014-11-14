b19258608da84115a57aacd03dc64ba2835dc514
Ashley Davis <dual@gmx.com>
2014-11-14 21:33:36 +0000

    Initial commit w/ skel

---
+++ server.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+var private = require("./private.js");
+var WebSocketClient = require('websocket').client;
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket client connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function() {
+        console.log('Socket Connection Closed');
+    });
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+});
+
+client.connect('wss://stream.pushbullet.com/websocket/'+private.pbAPIkey, null);
